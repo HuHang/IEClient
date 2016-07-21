203d5f73d0c6b322c31041d18e803bbdd551ad5a
HuHang <m18954237672_1@163.com>
2016-07-21 12:15:32 +0800

    new

---
+++ IEClient/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IEClient
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            double screenHeight = SystemParameters.FullPrimaryScreenHeight;
+            double screenWidth = SystemParameters.FullPrimaryScreenWidth;
+            this.Top = (screenHeight - this.Height) / 2;
+            this.Left = (screenWidth - this.Width) / 2;
+            getData();
+        }
+        void getData()
+        {
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            getData();
+            // MessageBox.Show("hh");
+            // Application.Current.Shutdown();
+            ItemsWindow win = new ItemsWindow();
+            //win.Content = new ResPage();
+            win.Show();
+            this.Close();
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
---
+++ IEClient/SettingBoxWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IEClient
+{
+    /// <summary>
+    /// SettingBoxWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingBoxWindow : Window
+    {
+        public SettingBoxWindow()
+        {
+            InitializeComponent();
+            //this.Topmost = true;
+            double screenHeight = SystemParameters.FullPrimaryScreenHeight;
+            double screenWidth = SystemParameters.FullPrimaryScreenWidth;
+            this.Top = (screenHeight - this.Height) / 2;
+            this.Left = (screenWidth - this.Width) / 2;
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+        private void cancel_Setting_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void save_Setting_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
